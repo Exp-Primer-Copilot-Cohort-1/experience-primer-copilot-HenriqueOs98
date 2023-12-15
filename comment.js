a192ae90794f573b3e5112079d2a498f7e425900
Henrique OS <57755206+HenriqueOs98@users.noreply.github.com>
2023-12-15 23:25:33 +0000

    Copilot third commit

--- comment.js
+++ comment.js
@@ -1,13 +1,17 @@
-function skillsMember() {
-    return {
-      restrict: 'E',
-      templateUrl: 'modules/skills/views/member.html',
-      controller: 'SkillsMemberController',
-      controllerAs: 'vm',
-      bindToController: true,
-      scope: {
-        member: '='
-      }
-    };
-  }
-  
+// Synthesizing 9/10 solutions (Duplicates hidden)
+// ========================================
+
+// Create web server for comment
+// ========================================
+
+// Import module
+const express = require('express');
+const router = express.Router();
+const commentController = require('../controllers/commentController');
+
+// Handle request
+router.get('/', commentController.comment_list);
+router.post('/create', commentController.comment_create_post);
+router.get('/create', commentController.comment_create_get);
+router.get('/:id/delete', commentController.comment_delete_get);
+module.exports = router;
